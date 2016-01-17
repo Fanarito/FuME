fdf1be1b441ee7ea7747cc1ab7ca489f92bcf094
Fanarito <viktor.saevars@gmail.com>
2016-01-17 20:05:31 +0000

    Added subtitle support and movie scanner now includes file metadata

--- routes/play.js
+++ routes/play.js
@@ -15,17 +15,17 @@ r.connect({
 });
 
 var playMovie = function(id, res, startTime) {
-	r.db('FuME').table('movies').get(id).pluck('fullpath').run(connection, function(err, cursor) {
+	r.db('FuME').table('movies').get(id).pluck('fullpath', 'subtitle_path').run(connection, function(err, cursor) {
 		if (err) {
 			return next(err);
 		}
 		console.log(cursor);
 
 		var proc = ffmpeg(cursor.fullpath)
-            .seekInput(startTime)
+      .seekInput(startTime)
 			.format('webm')
 			.videoCodec('libvpx')
-			.videoBitrate('2250k')
+			.videoBitrate('2000k')
 			.size('?x720')
 			.audioBitrate('128k')
 			.audioChannels(2)
@@ -39,6 +39,7 @@ var playMovie = function(id, res, startTime) {
 				'-speed 4',
 				'-deadline realtime'
 			])
+			.videoFilters('subtitles=' + cursor.subtitle_path)
 			.on('error', function(err, stdout, stderr) {
 				console.log('An error occurred: ' + err.message);
 				console.log('stdout: ' + stdout);
@@ -62,7 +63,7 @@ var playShow = function(id, res, season, episode, startTime) {
 		console.log(cursor);
 
 		var proc = ffmpeg(cursor.fullpath)
-            .seekInput(startTime)
+      .seekInput(startTime)
 			.format('webm')
 			.videoCodec('libvpx')
 			.videoBitrate('1500k')
@@ -71,6 +72,9 @@ var playShow = function(id, res, season, episode, startTime) {
 			.audioBitrate('128k')
 			.audioChannels(2)
 			.audioCodec('libvorbis')
+			.inputOptions([
+				'-re'
+			])
 			.outputOptions([
 				'-threads 8',
 				//'-cpu-used 16',
@@ -107,6 +111,11 @@ router.get('/movies/:id/:hour/:min/:sec/', function(req, res, next) {
 	playMovie(req.params.id, res, startTime);
 });
 
+router.get('/movies/subtitles/:id', function(req, res, next){
+	res.setHeader("content-type", "some/type");
+  fs.createReadStream("./toSomeFile").pipe(res);
+});
+
 router.get('/shows/:id/:season/:episode', function(req, res, next) {
     playShow(req.params.id, res, req.params.season, req.params.episode, "0:0:0");
 });
@@ -115,5 +124,6 @@ router.get('/shows/:id/:season/:episode/:hour/:min/:sec/', function(req, res, ne
     startTime = req.params.hour + ':' + req.params.min + ':' + req.params.sec;
     playShow(req.params.id, res, req.params.season, req.params.episode, startTime);
 });
+
 
 module.exports = router;
